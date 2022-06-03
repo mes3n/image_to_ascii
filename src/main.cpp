54b4a1eeb72fc5a0f27110eec8b91ceec012715a
mes3n <skidmackan@gmail.com>
2022-06-03 22:52:08 +0200

    added option to resize the ascii output

--- src/main.cpp
+++ src/main.cpp
@@ -15,8 +15,11 @@ extern "C" {
 
 int main (int argc, char **argv) {
 
-    std::string image_name;
+    std::string image_name = "";
     std::string output_name = "output/output.txt";
+
+    int sizex = 0;
+    int sizey = 0;
 
     for (int i = 1; i < argc; i++) {
 
@@ -49,11 +52,33 @@ int main (int argc, char **argv) {
                     return 0;
                 }
             }
+            else if (argv[i][1] == 's') {
+                if (i + 2 < argc) {
+                    try {
+                        sizex = std::stoi (argv[i + 1]);
+                        sizey = std::stoi (argv[i + 2]);
+                    }
+                    catch (std::invalid_argument &) {
+                        std::cout << "faulty arguments after " << argv[i] << "\n" \
+                        << "enter sizes as '-s 50 50'\n";
+                        return 0;
+                    }
+                }
+                else {
+                    std::cout << "missing argument after " << argv[i] << "\n";
+                    return 0;
+                }
+            }
             else {
                 std::cout << "unknown flag\n";
                 return 0;
             }
         }
+    }
+
+    if (image_name == "") {
+        std::cout << "missing image file to convert\n";
+        return 0;
     }
 
     int width, height;
@@ -70,27 +95,47 @@ int main (int argc, char **argv) {
         return 0;
     }
 
-    for (int y = 0; y < height; y++) {
-        for (int x = 0; x < width; x++) {
+    if (sizex > width || sizey > height) {
+        std::cout << "ascii art can not be larger than image\n";
+        return 0;
+    }
 
-            int index = 3*(y*width + x);
-            int r = static_cast<int>(data[index + 0]);  // red
-            int g = static_cast<int>(data[index + 1]);  // green
-            int b = static_cast<int>(data[index + 2]);  // blue
+    sizex = sizex ? sizex : width;
+    sizey = sizey ? sizey : height;
 
-            int a = static_cast<int>(round(((0.299*r + 0.587*g + 0.114*b)/255)*ascii::count));  // grayscale value of the pixel
+    int xpixel_per_ascii = static_cast<int>(round(width / sizex));
+    int ypixel_per_ascii = static_cast<int>(round(height / sizey));
 
-            if (a == count) {  // the char ' ' doesnt seem to render properely
-                std::cout << "  ";
+    for (int y = 0; y < sizey; y++) {
+        for (int x = 0; x < sizex; x++) {
+
+            int r = 0;
+            int g = 0;
+            int b = 0;
+
+            for (int px = 0; px < xpixel_per_ascii; px++) {
+                for (int py = 0; py < ypixel_per_ascii; py++) {
+
+                    int index = 3*((y*ypixel_per_ascii + py)*width + (x*xpixel_per_ascii + px));
+
+                    r += static_cast<int>(data[index + 0]);  // red
+                    g += static_cast<int>(data[index + 1]);  // green
+                    b += static_cast<int>(data[index + 2]);  // blue
+
+                }
+            }
+
+            // 0.299*r + 0.587*g + 0.114*b
+            int pixel_count = xpixel_per_ascii*ypixel_per_ascii;
+            int a = static_cast<int>(round(((0.333*r + 0.333*g + 0.333*b)/(255*pixel_count))*ascii::count));  // grayscale value of the pixel
+
+            if (a == ascii::count) {  // the char ' ' doesnt seem to render properely
                 output << "  ";
             }
             else {
-                std::cout << ascii::characters[a] << " ";
                 output << ascii::characters[a] << " ";
             }
-
         }
-        std::cout << "\n";
         output << "\n";
     }
 
